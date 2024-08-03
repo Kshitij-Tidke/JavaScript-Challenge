4fd01aa9fd4892cd690e777c5ac3e1cb279f2a3e
Kshitij-Tidke <ktidke005@gmail.com>
2024-08-03 23:16:38 +0530

    Day 1 assignment are completed

--- Day-1/taskOne.js
+++ Day-1/taskOne.js
@@ -1,1 +1,47 @@
-console.log("hello world!");
+// Day 1: Variables and Data Types
+// Activity 1: Variables Declaration    
+
+// Task 1:
+var variableOne = 10
+console.log(variableOne);
+
+// Task 2:
+let variableTwo = "Kshitij"
+console.log(variableTwo);
+
+// Activity 2: Constant Declaration
+
+// Task 3:
+const variableThree = true   
+console.log(variableThree);
+
+// Activity 3: Data Types
+
+// Task 4: 
+let variableFour = 10.10
+let variableFive = "Tidke"
+let variableSix = false
+const objOne = {
+    name: "Kshitij"
+}
+const arrOne = [1,2,3,4,5]
+
+console.log(`${variableFour} type is ${typeof variableFour}`);
+console.log(`${variableFive} type is ${typeof variableFive}`);
+console.log(`${variableSix} type is ${typeof variableSix}`);
+console.log(`${objOne} type is ${typeof objOne}`);
+console.log(`${arrOne} type is ${typeof arrOne}`);
+
+// Activity 4: Reassigning Variables
+
+// Task 5:
+let variableSeven = 10
+console.log(variableSeven);
+variableSeven = 20
+console.log(variableSeven);
+
+// Activity 5: Understanding const
+const variableEight = 10
+console.log(variableEight);
+//variableEight = 20 // Assignment to constant variable.
+// console.log(variableEight);
